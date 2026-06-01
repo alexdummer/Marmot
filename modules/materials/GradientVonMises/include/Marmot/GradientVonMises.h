9112d6228e8e0f0aaf2094031a04e0a0646c4129
Alexander Dummer <alexander.dummer@uibk.ac.at>
2026-06-01 09:43:34 -0600

    add gradient plasticity material prototype

---
+++ modules/materials/GradientVonMises/include/Marmot/GradientVonMises.h
@@ -0,0 +1,95 @@
+/* ---------------------------------------------------------------------
+ *                                       _
+ *  _ __ ___   __ _ _ __ _ __ ___   ___ | |_
+ * | '_ ` _ \ / _` | '__| '_ ` _ \ / _ \| __|
+ * | | | | | | (_| | |  | | | | | | (_) | |_
+ * |_| |_| |_|\__,_|_|  |_| |_| |_|\___/ \__|
+ *
+ * Unit of Strength of Materials and Structural Analysis
+ * University of Innsbruck,
+ * 2020 - today
+ *
+ * festigkeitslehre@uibk.ac.at
+ *
+ * This file is part of the MAteRialMOdellingToolbox (marmot).
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * The full text of the license can be found in the file LICENSE.md at
+ * the top level directory of marmot.
+ * ---------------------------------------------------------------------
+ */
+
+#pragma once
+#include "Marmot/MarmotMaterialGradientPlasticityHypoElastic.h"
+#include "Marmot/MarmotTypedefs.h"
+#include "Marmot/MarmotVoigt.h"
+
+namespace Marmot::Materials {
+
+  class GradientVonMises : public MarmotMaterialGradientPlasticityHypoElastic< 1 > {
+
+  public:
+    GradientVonMises( const double* materialProperties, int nMaterialProperties, int materialNumber );
+
+    void computeStress( response& res, tangents& tan, const increment& inc ) const override;
+
+    void initializeStateLayout()
+    {
+      stateLayout.add( "kappa", 1 );
+      stateLayout.add( "laplaceKappa", 1 );
+      stateLayout.finalize();
+    }
+
+    double getDensity( const double* stateVars ) const override;
+
+    std::vector< double > getNonlocalViscosity( const double* stateVars ) const override;
+
+  private:
+    /// @brief Elastic stiffness tensor
+    const Marmot::Matrix6d C;
+    const double&          fy0; // initial yield strength
+    const double&          H;   // hardening modulus
+    const double&          g;   // gradient influence parameter
+
+    /// compute yield stress and its derivatives from kappa and laplaceKappa
+    std::tuple< double, double, double > fy( double kappa, double laplaceKappa ) const
+    {
+      const double sigmaY = fy0 + H * kappa - g * laplaceKappa; // yield stress as a function of kappa
+      return { sigmaY,
+               H,
+               -g }; // return yield stress and its derivatives (d(sigmaY)/d(kappa) = H, d(sigmaY)/d(laplaceKappa) = -g)
+    }
+
+    // compute the von Mises yield function value and its derivatives with respect to stress, kappa, and laplaceKappa
+    std::tuple< double, Vector6d, Matrix6d, double, double > yieldFunction( const Vector6d& stress,
+                                                                            double&         kappa,
+                                                                            double&         laplaceKappa ) const
+    {
+      using namespace Marmot::ContinuumMechanics::VoigtNotation;
+      const auto [sigmaY, dSigmaY_dKappa, dSigmaY_dLaplaceKappa] = fy( kappa, laplaceKappa );
+      const double   J2                                          = Invariants::J2( stress );
+      const Vector6d dJ2_dStress                                 = Derivatives::dJ2_dStress( stress );
+      const Matrix6d d2J2_dStress2                               = Derivatives::d2J2_dStress2( stress );
+
+      const Vector6d dF_ddStress  = dJ2_dStress * ( 3.0 / ( 2.0 * std::sqrt( 3.0 * J2 ) ) );
+      const Matrix6d d2F_dStress2 = d2J2_dStress2 * ( 3.0 / ( 2.0 * std::sqrt( 3.0 * J2 ) ) ) -
+                                    ( dJ2_dStress * dJ2_dStress.transpose() ) * ( 9.0 / ( 8.0 * std::pow( J2, 1.5 ) ) );
+
+      const double f = std::sqrt( 3.0 * J2 ) - sigmaY; // yield
+
+      return { f, dF_ddStress, d2F_dStress2, -dSigmaY_dKappa, -dSigmaY_dLaplaceKappa };
+    }
+
+    // check if yielding
+    bool isYielding( const Vector6d& stress, double kappa, double laplaceKappa ) const
+    {
+      const auto [f, _, __, ___, ____] = yieldFunction( stress, kappa, laplaceKappa );
+      return f > 1e-14;
+    }
+  };
+
+} // namespace Marmot::Materials
---
+++ modules/materials/GradientVonMises/src/GradientVonMises.cpp
@@ -0,0 +1,96 @@
+#include "Marmot/GradientVonMises.h"
+#include "Marmot/MarmotElasticity.h"
+#include "Marmot/MarmotPhaseFieldEnergyDegradation.h"
+#include "Marmot/MarmotTypedefs.h"
+
+namespace Marmot::Materials {
+
+  using namespace Eigen;
+  using namespace Marmot;
+
+  GradientVonMises::GradientVonMises( const double* materialProperties, int nMaterialProperties, int materialNumber )
+    : MarmotMaterialGradientPlasticityHypoElastic< 1 >( materialProperties, nMaterialProperties, materialNumber ),
+      C( ContinuumMechanics::Elasticity::Isotropic::stiffnessTensor( materialProperties[0], materialProperties[1] ) ),
+      fy0( materialProperties[2] ),
+      H( materialProperties[3] ),
+      g( materialProperties[4] )
+  {
+    initializeStateLayout();
+  }
+
+  double GradientVonMises::getDensity( const double* stateVars ) const
+  {
+    if ( nMaterialProperties <= 5 )
+      throw std::runtime_error( "Density not provided in material properties for GradientVonMises." );
+    return materialProperties[5];
+  }
+
+  std::vector< double > GradientVonMises::getNonlocalViscosity( const double* stateVars ) const
+  {
+    if ( nMaterialProperties <= 6 )
+      throw std::runtime_error( "Viscosity not provided in material properties for GradientVonMises." );
+    return { materialProperties[6] };
+  }
+  void GradientVonMises::computeStress( response& res, tangents& tan, const increment& inc ) const
+  {
+
+    // map response and increment variables for easier access
+    Vector6d&       stress  = res.stress;
+    const Vector6d& dStrain = inc.dStrain;
+    double&         f       = res.f( 0 ); // yield function value
+
+    // map to tangents
+    Matrix6d&               dStressddStrain  = tan.dStressddStrain;
+    Matrix< double, 6, 1 >& dStressddLambda  = tan.dStressddLambda;
+    Matrix< double, 1, 6 >& dF_ddStrain      = tan.dFddStrain;
+    double&                 dF_dKappa        = tan.dFddLambda( 0, 0 );
+    double&                 dF_dLaplaceKappa = tan.dFddLambda( 0, 0 );
+
+    // get state variables
+    double& kappa        = stateLayout.getAs< double& >( res.stateVars, "kappa" );
+    double& laplaceKappa = stateLayout.getAs< double& >( res.stateVars, "laplaceKappa" );
+
+    // update kappa and laplaceKappa
+    kappa += inc.dLambda( 0 );
+    laplaceKappa += inc.laplaceDLambda( 0 );
+
+    // compute trial stress
+    Vector6d trialStress = stress + C * dStrain;
+
+    if ( isYielding( trialStress, kappa, laplaceKappa ) ) {
+      // plastic correction
+      const double& dLambda = inc.dLambda( 0 );
+
+      const auto [f_tr,
+                  dF_dStress_tr,
+                  d2F_dStress2_tr,
+                  dF_dKappa_tr,
+                  dF_dLaplaceKappa_tr] = yieldFunction( trialStress, kappa, laplaceKappa );
+      // update stress with trial return mapping direction
+      stress = trialStress - C * ( dLambda * dF_dStress_tr );
+
+      dStressddStrain = C - dLambda * C * d2F_dStress2_tr * C;
+      dStressddLambda = -C * dF_dStress_tr;
+
+      Vector6d dF_dStress;
+      Matrix6d d2F_dStress2;
+      std::tie( f, dF_dStress, d2F_dStress2, dF_dKappa, dF_dLaplaceKappa ) = yieldFunction( stress,
+                                                                                            kappa,
+                                                                                            laplaceKappa );
+      dF_ddStrain                                                          = dF_dStress.transpose() * C;
+    }
+    else {
+      // elastic step
+      stress = trialStress;
+      Vector6d dF_dStress;
+      Matrix6d d2F_dStress2;
+      std::tie( f, dF_dStress, d2F_dStress2, dF_dKappa, dF_dLaplaceKappa ) = yieldFunction( stress,
+                                                                                            kappa,
+                                                                                            laplaceKappa );
+      // fill tangents for elastic step
+      dF_ddStrain     = dF_dStress.transpose() * C;
+      dStressddStrain = C;
+    }
+  }
+
+} // namespace Marmot::Materials
